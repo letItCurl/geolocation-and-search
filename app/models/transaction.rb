ee519725e472fd2afd7103b0f5e2a1ef11f8bb89
letItCurl <rolandlopez.developer@gmail.com>
2020-11-05 09:26:34 +0700

    data indexed in elastic search

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,6 +1,7 @@
 class Transaction < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :address_changed?
+  searchkick locations: [:location]
 
   def address
     [street, city, zip, state].compact.join(", ")
@@ -8,5 +9,9 @@ class Transaction < ApplicationRecord
 
   def address_changed?
     street_changed? |  city_changed? |  zip_changed? |  state_changed?
+  end
+
+  def search_data
+    attributes.merge location: {lat: latitude, lon: longitude}
   end
 end
